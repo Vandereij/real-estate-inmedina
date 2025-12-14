f0b24219e89e544ac310630248a58e6425af27c4
Stefano <stefano.dirella@gmail.com>
2025-12-14 21:50:20 +0000

    Add tag manager to head

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,7 +3,7 @@ import Header from "@/components/header";
 import Footer from "@/components/footer";
 import "./globals.css";
 import { createSupabaseServerClient } from "@/lib/auth";
-import { GoogleTagManager } from '@next/third-parties/google';
+import { GoogleTagManager } from "@next/third-parties/google";
 
 export default async function RootLayout({
   children,
@@ -11,13 +11,11 @@ export default async function RootLayout({
   children: React.ReactNode;
 }) {
   const supabase = await createSupabaseServerClient();
-
   const {
     data: { user },
   } = await supabase.auth.getUser();
 
   let isAdmin = false;
-
   if (user) {
     const { data } = await supabase.rpc("is_admin");
     isAdmin = !!data;
@@ -25,10 +23,11 @@ export default async function RootLayout({
 
   return (
     <html lang="en">
-      <GoogleTagManager gtmId="G-XTDJH4XHV1" />
+      <head>
+        <GoogleTagManager gtmId={process.env.NEXT_PUBLIC_GTM_ID!} />
+      </head>
       <body className="min-h-screen bg-background text-[#1e1e1e]">
-        <main className="flex flex-col min-h-screen">
-          {/* pass admin + user info down */}
+        <main className="flex min-h-screen flex-col">
           <Header isAdmin={isAdmin} user={user} />
           <section className="flex-1">{children}</section>
           <Footer />
