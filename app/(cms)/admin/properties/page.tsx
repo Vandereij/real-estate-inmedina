63d9b03c0daf773c64e9a15ad7584b02d5bde3f4
Stefano <stefano.dirella@gmail.com>
2025-12-27 21:08:40 +0000

    Change style of H1 in admin properties

--- app/(cms)/admin/properties/page.tsx
+++ app/(cms)/admin/properties/page.tsx
@@ -102,8 +102,8 @@ export default async function PropertiesPage({
 		<section>
 			{/* 🏠 Properties List (Strips) */}
 			<div className="mx-auto grid max-w-7xl px-4 pt-10 pb-16">
-				<h2 className="text-xl md:text-2xl mb-10">All Properties</h2>
-
+				<h1 className="text-xl md:text-2xl mb-10 font-semibold">All Properties</h1>
+				
 				{/* Strips instead of cards */}
 				<div className="flex flex-col gap-4">
 					{data?.map((p: any) => (
