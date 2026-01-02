4e04f4f9f41510cf6ddb226f7392ac9b995c084f
Stefano <stefano.dirella@gmail.com>
2026-01-02 00:03:20 +0000

    Change loading of image in property

--- components/property-gallery.tsx
+++ components/property-gallery.tsx
@@ -114,7 +114,10 @@ export function PropertyGallery({
 						</span>
 					</h2>
 				</div>
-				<img
+				<motion.img
+					initial={{ opacity: 0, scale: 0.98 }}
+					animate={{ opacity: 1, scale: 1 }}
+					transition={{ duration: 0.25 }}
 					src="/placeholder.png"
 					alt="Property placeholder"
 					loading="lazy"
