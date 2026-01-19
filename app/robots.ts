787c12ff239309183887b222c731d41d5aed5a9a
Stefano <stefano.dirella@gmail.com>
2026-01-19 11:46:12 +0000

    Revert "Changes to index pages in google"
    This reverts commit e993d5aa697958e54afdcac374417fd2d223bf29.

--- app/robots.ts
+++ app/robots.ts
@@ -2,10 +2,7 @@ import type { MetadataRoute } from "next";
 
 export default function robots(): MetadataRoute.Robots {
   return {
-    rules: [
-      { userAgent: "*", allow: "/" },
-      { userAgent: "*", disallow: ["/admin", "/auth", "/unauthorized"] },
-    ],
+    rules: [{ userAgent: "*", allow: "/" }],
     sitemap: "https://realestate.inmedina.com/sitemap.xml",
   };
 }
